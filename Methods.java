9fde2a74baeb1e70cf45ecb1396dd4eeb7b5b18e
Runbier Sabhlok <superrunrocks@gmail.com>
2022-09-11 19:44:20 -0400

    Finished part one of the project. Part two is putting all of it inside a window in the computer and make it easier for the user to use (JFrame).

--- Methods.java
+++ Methods.java
@@ -1,2 +1,20 @@
 public class Methods {
+    public void quadForm(int a, int b, int c){
+
+        double discriminant = Math.sqrt(b*b - (4 * a * c));
+        int nigB = Math.negateExact(b);
+
+        double numerator = nigB - discriminant;
+        double numerator2 = nigB + discriminant;
+
+        int denominator = 2 * a;
+
+        double answer = numerator / denominator;
+        double answer2 = numerator2 / denominator;
+
+        System.out.print(answer);
+        System.out.print(" and ");
+        System.out.print(answer2);
+        System.out.println(" ");
+    }
 }
--- main.java
+++ main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class main {
     public static void main(String []args){
         Methods methods = new Methods();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value for a: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter value for b: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Enter value for c: ");
+        int c = scanner.nextInt();
+
+        System.out.print("The answer to the quadratic equation is: ");
+        methods.quadForm(a,b,c);
     }
 }
